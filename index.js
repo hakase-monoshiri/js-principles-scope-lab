b18007316d8fe9097fd046b63e5d5baba7643868
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-15 21:45:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = "Jyaboi";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+};
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+    return bestCustomer;
+};
+
+function overwriteBestCustomer(new_best_customer) {
+    bestCustomer = new_best_customer;
+    return bestCustomer;
+};
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "commander";
+};
